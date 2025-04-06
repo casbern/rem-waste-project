5ea86c758e0b45c2629b918c13d8bae3205a8135
Cássia <cascrisbern@gmail.com>
2025-04-06 19:42:45 +0300

    add: new images

--- src/data/skipExtraData.js
+++ src/data/skipExtraData.js
@@ -2,31 +2,31 @@
 export const skipExtraData = {
 
   4 :{
-    img: "/images/4-Yard-skip.jpg",
+    img: "/images/4-Yard-skip.png",
     description: ["Normally known as 'Midi Skips'", "It's a great size for small domestic jobs", "It holds around 30-40 bin bags"]
   },
   6: {
-    img: "./images/6-Yard-skip.jpg",
+    img: "./images/6-Yard-skip.png",
     description: ["Referred to 'Builders Skips'", "It holds around 50-60 bin bags", "Great for harcore waste"],
   },
   8: {
-    img: "./images/8-Yard-skip.jpg",
+    img: "./images/8-Yard-skip.png",
     description: ["Referred to as 'Builders Skips'", "Great for hardcore waste", "It holds around 60-80 bin bag of waste"]
   },
   10: {
-    img: "./images/10-Yard-skip.jpg",
+    img: "./images/10-Yard-skip.png",
     description: ["It is great for big cleanouts and bulky waste", "It holds around 80-100 bin bags of waste"]
   },
   12: {
-    img: "./images/12-Yard-skip.jpg",
+    img: "./images/12-Yard-skip.png",
     description: ["Great for large business or house clear outs", "It holds around 100-120 bin bags", "Not suitable for heavy hardcore", "It has width restriction"]
   },
   14: {
-    img: "./images/20-Yard-skip.png",
+    img: "./images/12-Yard-skip.png",
     description: ["Lorem Ipsum", "Lorem Ipsum"]
   },
   16: {
-    img: "./images/20-Yard-skip.png",
+    img: "./images/12-Yard-skip.png",
     description: ["Lorem Ipsum", "Lorem Ipsum"]
   },
   20: {
